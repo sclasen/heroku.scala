3f961df7f9fa6277d8359f11777f0eb45b6b76d2
Scott Clasen <scott@heroku.com>
2013-03-08 23:00:07 -0800

    account spec

---
+++ spray-client/src/test/scala/com/heroku/api/spray/AccountSpec.scala
@@ -0,0 +1,30 @@
+package com.heroku.api.spray
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+import akka.actor.ActorSystem
+import com.heroku.api._
+import com.heroku.api.CreateApp
+import com.heroku.api.AppUpdate
+import com.heroku.api.AppList
+import scala.Some
+import com.heroku.api.AppCreate
+
+
+class AccountSpec extends WordSpec with MustMatchers {
+
+
+  "Spray Api implementation of Account operations" must {
+    "compile" in {
+      import SprayApi._
+      val system = ActorSystem("test")
+      val api = new SprayApi(system)
+      val key = "foo"
+      val update = AccountUpdate(UpdateAccount(allow_tracking = Some(false)))
+      api.execute(update, key)
+      val info = AccountInfo()
+      api.execute(info, key)
+    }
+  }
+
+}
