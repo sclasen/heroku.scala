03ca4344012a8d07dc2f4269f2dc36a81b922d5b
Scott Clasen <scott@heroku.com>
2013-11-08 15:18:31 -0800

    slug commit is nullable, add SlugSpec, and testing slug

---
+++ api/src/test/scala/com/heroku/platform/api/SlugSpec.scala
@@ -0,0 +1,22 @@
+package com.heroku.platform.api
+
+abstract class SlugSpec(aj: ApiRequestJson with ApiResponseJson) extends ApiSpec(aj) {
+
+  val implicits: SlugRequestJson with SlugResponseJson = aj
+
+  import implicits._
+
+  "Api for Slugs" must {
+    "operate on Slugs" in {
+      import primary._
+      val app = getApp
+      val slug = request(Slug.Create(app.id, None, Map("clock" -> "bin/clock", "echo" -> "bin/echo")))
+      println(slug.blob)
+      val info = request(Slug.Info(app.id, slug.id))
+      info.id must equal(slug.id)
+    }
+
+  }
+
+}
+
